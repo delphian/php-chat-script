fdd7cc9859d7adf2c6d0ae02434f943a5b597d9e
delphian <delphian@cl0.beta.service.aws.koding.com>
2012-09-16 19:28:13 -0700

    .htaccess will be auto installed if it is missing. This allows dev sites to modify the htaccess without corrupting the repo.

--- server.php
+++ server.php
@@ -7,39 +7,41 @@
  * Licensed under the MIT license.
  */
 
-$config = array(
-  'path_root'    => preg_replace('/\/[^\/]*$/', '', __FILE__),
-  'path_inc'     => preg_replace('/\/[^\/]*$/', '', __FILE__) . '/inc',
-  'path_data'    => preg_replace('/\/[^\/]*$/', '', __FILE__) . '/data',
-);
-
-// Turn on all error reporting for a development environment.
+/** Turn on all error reporting for a development environment. */
 ini_set('error_reporting', E_ALL);
 
+/** If this is our first time running install the default .htaccess.
+    @todo This entire first time install thing should be handled better. */
+if (!file_exists('.htaccess')) {
+  if (!copy('install_files/install_htaccess', '.htaccess')) {
+    throw new Exception('Can not install htaccess file.');
+  }
+}
 
+/** Include our class (backend) libraries. */
+require_once('inc/SingletonLoader/SingletonLoader.php');
+require_once('inc/Observed/Observed.php');
+require_once('inc/SimpleTextStorage/SimpleTextStorage.php');
+require_once('inc/SimpleUser/SimpleUser.php');
+require_once('inc/Chat/Chat.php');
+require_once('inc/Plugin.php');
+require_once('inc/Server.php');
 
-require_once($config['path_inc'] . '/SingletonLoader/SingletonLoader.php');
-require_once($config['path_inc'] . '/Observed/Observed.php');
-require_once($config['path_inc'] . '/SimpleTextStorage/SimpleTextStorage.php');
-require_once($config['path_inc'] . '/SimpleUser/SimpleUser.php');
-require_once($config['path_inc'] . '/Chat/Chat.php');
-require_once($config['path_inc'] . '/Plugin.php');
-require_once($config['path_inc'] . '/Server.php');
-
-require_once($config['path_inc'] . '/Cli/Cli.php');
-require_once($config['path_inc'] . '/Chat/ChatPlugin.php');
-require_once($config['path_inc'] . '/SimpleUser/SimpleUserPlugin.php');
+/** Include our plugins. */
+require_once('inc/Cli/Cli.php');
+require_once('inc/Chat/ChatPlugin.php');
+require_once('inc/SimpleUser/SimpleUserPlugin.php');
 
 /**
  * Begin running server.
  */
-// Create server.
+/** Create server. */
 $server = new Server();
-// Get the request from $_GET, $_POST, or a plugin.
+/** Get the request from $_GET, $_POST, or a plugin. */
 $server->receive_request();
-// Run the server. Invoke all plugins registered for this code.
+/** Run the server. Invoke all plugins registered for this code. */
 $server->process_request();
-// Close down the server gracefully.
+/** Close down the server gracefully. */
 $server->halt();
 
 ?>
